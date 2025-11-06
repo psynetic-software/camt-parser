e59261766983228dd02ce6aec9888f65d4713944
Psynetic <119499591+psynetic@users.noreply.github.com>
2025-11-06 23:23:08 +0100

    Example changed

--- examples/qt-csv-export-demo/main.cpp
+++ examples/qt-csv-export-demo/main.cpp
@@ -36,9 +36,20 @@ int main(int argc, char *argv[])
 
     for (const auto& row : camtData)
     {
-        qDebug()    << "ValueDate:"    <<  row[camt::to_index(camt::ExportField::ValueDate)].second
-                    << "BookingDate:"  <<  row[camt::to_index(camt::ExportField::BookingDate)].second
-                    << "Amount:"       <<  row[camt::to_index(camt::ExportField::Amount)].second;
+        auto S = [](const std::string& s){ return QString::fromUtf8(s.c_str()); };
+
+        qDebug()    << "CounterpartyIBAN:"                          <<  S(row[camt::to_index(camt::ExportField::CounterpartyIBAN)].first);
+        qDebug()    << "RemittanceLine:"                            <<  S(row[camt::to_index(camt::ExportField::RemittanceLine)].first);
+        qDebug()    << "IsCredit:"                                  <<  row[camt::to_index(camt::ExportField::CreditDebit)].first;
+        qDebug()    << "Reversal:"                                  <<  row[camt::to_index(camt::ExportField::Reversal)].first;
+
+        qDebug()    << "ValueDate   YYYY-MM-DD:"                    <<  S(row[camt::to_index(camt::ExportField::ValueDate)].first)
+                    << "ValueDate   YYYYMMDD:"                      <<  S(row[camt::to_index(camt::ExportField::ValueDate)].second)     << "\n"
+                    << "BookingDate YYYY-MM-DD:"                    <<  S(row[camt::to_index(camt::ExportField::BookingDate)].first)
+                    << "BookingDate YYYYMMDD:"                      <<  S(row[camt::to_index(camt::ExportField::BookingDate)].second)   << "\n"
+                    << "Amount before CreditDebit and Reversal:"    <<  S(row[camt::to_index(camt::ExportField::Amount)].second)
+                    << "Final Amount:"                              <<  S(row[camt::to_index(camt::ExportField::Amount)].first)
+                    << "\n";
     }
 
     qInfo("Done.");
